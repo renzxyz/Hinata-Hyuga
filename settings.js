22cc246779c32b80f1431a4358c1031140da2a1b
renzxyz <106762944+renzxyz@users.noreply.github.com>
2022-06-07 20:50:08 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,27 +14,27 @@
   // Edit Sesuai Kemauanmu
   global.autoReadGc = true // Ubah Ke false Jika Chat Di Grup Tidak Ingin Di Baca Oleh Bot
   global.autoReadAll = false // Ubah Ke true Jika Ingin Seluruh Chat Di Baca Oleh Bot
-  global.anticall = true // Ubah Ke false Jika Tidak Ingin Bot Blockir Yang Telepon
+  global.anticall = false // Ubah Ke false Jika Tidak Ingin Bot Blockir Yang Telepon
   global.available = false // Ubah Ke false Jika Bot Mu Tidak Ingin Terlihat Online
   global.autoTyping = false // Ubah Ke true Jika Ingin Saat Ada Yang Chat Bot Auto Mengetik
-  global.autoRecord = true // Ubah Ke false Jika Tidak Ingin Saat Ada Yang Chat Bot Auto Merekam
+  global.autoRecord = false // Ubah Ke false Jika Tidak Ingin Saat Ada Yang Chat Bot Auto Merekam
   global.multiplier = 69 // Semakin Besar Angkanya Semakin Sulit Untuk Naik Level
-  global.limitCount = 70 // 70 Dah Pas Banh
+  global.limitCount = 99 // 70 Dah Pas Banh
   global.prefa = ['','.','/','#']
 
   // Other
-  global.ownername = 'Riy' // Ganti Jadi Namamu
-  global.owner = ['6281575886399','6281575886399','6285813334446'] // Kalo Mau Lebih Tambahin Aja
-  global.ownernomer = '6281575886399' // Owner Utama Istilahnya
-  global.botname = 'Hinata Hyuga' // Ganti Jadi Nama Botmu
-  global.packname = 'Hinata Hyuga' // Sticker Weem
-  global.author = 'Riy' // Sticker Weem
-  global.webme = 'https://api-riy.herokuapp.com' // Ganti Sama Web Mu
-  global.linkgroup = 'https://chat.whatsapp.com/HZhLynp6E9QKNrRFtUp26T' // Ganti Sama Link Gc Mu
+  global.ownername = 'RenzXz' // Ganti Jadi Namamu
+  global.owner = ['62895360953898','62895360953898','62895360953898'] // Kalo Mau Lebih Tambahin Aja
+  global.ownernomer = '62895360953898' // Owner Utama Istilahnya
+  global.botname = '日向ヒナタ' // Ganti Jadi Nama Botmu
+  global.packname = 'Renz Ganteng' // Sticker Weem
+  global.author = 'Renz Ganteng' // Sticker Weem
+  global.webme = 'https://api-renzxyz.herokuapp.com' // Ganti Sama Web Mu
+  global.linkgroup = 'https://chat.whatsapp.com/JV52nSWnL7lK6BGLbm0JqT' // Ganti Sama Link Gc Mu
   global.sessionName = 'session'
-  global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw' // Ganti Sama Link Yt Mu
-  global.github = 'https://github.com/riychdwayne' // Ganti Sama Link Github Mu
-  global.instagram = 'https://instagram.com/riychdwayne' // Ganti Sama Link Ig mu
+  global.youtube = 'https://youtube.com/channel/UCK6GHLFDR4E1I3GpTGjeesg' // Ganti Sama Link Yt Mu
+  global.github = 'https://github.com/renzxyz' // Ganti Sama Link Github Mu
+  global.instagram = 'https://instagram.com/renz.shop01' // Ganti Sama Link Ig mu
 
   // Background Setiap Menu
   global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
